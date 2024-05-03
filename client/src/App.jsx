7183eadbdc1ca3740b6ae09362b8daf6781466de
Najwa4 <najjwa0000@gmail.com>
2024-05-03 13:03:12 +0300

    profile upadtes and add password change and reporte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -53,6 +53,7 @@ function App() {
         <Route path="/Test" element={<UpdateTestDayPage />} />
 
         <Route path="/Prof" element={<HRStaffProfileCard />} />
+        
       </Routes>
     </BrowserRouter>
   );
